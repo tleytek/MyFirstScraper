753982455f5d627b38aad57e3b9c05125a636fbc
tleytek <lucidmat94@gmail.com>
2018-10-25 17:10:53 -0400

    dropped the modal idea because lel handlebars sucks, doing seperate comment page

--- controller/scraperController.js
+++ controller/scraperController.js
@@ -22,21 +22,25 @@ router.get("/", function(req, res) {
 		});
 });
 
-// // Route for grabbing a specific Article by id, populate it with it's note
-// router.get("/articles/:id", function(req, res) {
-// 	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-// 	db.Article.findOne({ _id: req.params.id })
-// 		// ..and populate all of the notes associated with it
-// 		.populate("comment")
-// 		.then(function(dbArticle) {
-// 			// If we were able to successfully find an Article with the given id, send it back to the client
-// 			res.json(dbArticle);
-// 		})
-// 		.catch(function(err) {
-// 			// If an error occurred, send it to the client
-// 			res.json(err);
-// 		});
-// });
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function(req, res) {
+	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+	db.Article.findOne({ _id: req.params.id })
+		// ..and populate all of the notes associated with it
+		.populate("comment")
+		.then(function(dbArticle) {
+			console.log(dbArticle);
+			var singleArticle = {
+				single: dbArticle
+			};
+			// If we were able to successfully find an Article with the given id, send it back to the client
+			res.render("comments", singleArticle);
+		})
+		.catch(function(err) {
+			// If an error occurred, send it to the client
+			res.json(err);
+		});
+});
 
 // // Route for saving/updating an Article's associated Note
 // router.post("/articles/:id", function(req, res) {
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,59 +2,14 @@ var getComments = function(thisId) {
 	$.ajax({
 		method: "GET",
 		url: "/articles/" + thisId
-	}).then(function(data) {
-		$(".modal-title").text(data.title);
-		$("#saveComment").attr("data-id", data._id);
-		$("#current-title").empty();
-		$("#current-comment").empty();
-		// If there's a comment in the article
-		if (data.comment) {
-			console.log(data.comment);
-			$("#current-title").append(data.comment.title);
-			$("#current-comment").append(data.comment.text);
-			// Place the name of the user in the name id and the comment in the comment id
-			// Place the body of the comment in the body textarea
-		}
 	});
 };
 
-// $.getJSON("/", function(data) {
-// 	for (var i = 0; i < data.length; i++) {
-// 		//I really don't like this appending html cluster ff
-// 		$("#articles").append(
-// 			`<div class="row mt-4">
-//                 <div class="col-3">
-//                     <img class="rounded thumbnail"
-//                     style="width:100%"
-//                     src='${data[i].image}' alt='article image'>
-//                 </div>
-//                 <div class="col-9">
-//                     <a  href='${data[i].link}'
-//                         target='_blank'>
-//                             <h3 class='mx-auto text-center'>
-//                             ${data[i].title}
-//                             </h3>
-//                     </a>
-//                     <p class="mb-0"> ${data[i].text}</p>
-//                     <button
-//                         type="button"
-//                         class="btn btn-primary float-right"
-//                         data-id='${data[i]._id}'
-//                         data-toggle="modal"
-//                         data-target=".bd-example-modal-lg"
-//                         id="commentButton"
-//                     >Comment</button>
-//                 </div>
-//             </div>`
-// 		);
-// 	}
+// $(document).on("click", "#commentButton", function() {
+// 	var thisId = $(this).attr("data-id");
+// 	// Now make an ajax call for the Article
+// 	getComments(thisId);
 // });
-
-$(document).on("click", "#commentButton", function() {
-	var thisId = $(this).attr("data-id");
-	// Now make an ajax call for the Article
-	getComments(thisId);
-});
 
 // When you click the save comment button
 $(document).on("click", "#saveComment", function() {
