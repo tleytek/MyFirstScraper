b0492bc31ae5e297a84f09a3f3d38500344631cd
tleytek <lucidmat94@gmail.com>
2018-10-28 12:38:58 -0400

    working comments w/ handlebars

--- controller/scraperController.js
+++ controller/scraperController.js
@@ -42,31 +42,30 @@ router.get("/articles/:id", function(req, res) {
 		});
 });
 
-// // Route for saving/updating an Article's associated Note
-// router.post("/articles/:id", function(req, res) {
-// 	console.log(req.body);
-// 	// Create a new note and pass the req.body to the entry
-// 	db.Comment.create(req.body)
-// 		.then(function(dbComment) {
-// 			console.log(dbComment);
-// 			// If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-// 			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-// 			// Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-// 			return db.Article.findOneAndUpdate(
-// 				{ _id: req.params.id },
-// 				{ comment: dbComment._id },
-// 				{ new: true }
-// 			);
-// 		})
-// 		.then(function(dbArticle) {
-// 			// If we were able to successfully update an Article, send it back to the client
-// 			res.json(dbArticle);
-// 		})
-// 		.catch(function(err) {
-// 			// If an error occurred, send it to the client
-// 			res.json(err);
-// 		});
-// });
+// Route for saving/updating an Article's associated Note
+router.post("/articles/:id", function(req, res) {
+	console.log(req.body);
+	// Create a new note and pass the req.body to the entry
+	db.Comment.create(req.body)
+		.then(function(dbComment) {
+			console.log(dbComment);
+			// If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+			// Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+			return db.Article.findOneAndUpdate(
+				{ _id: req.params.id },
+				{ comment: dbComment._id }
+			);
+		})
+		.then(function(dbArticle) {
+			// If we were able to successfully update an Article, send it back to the client
+			res.json(dbArticle);
+		})
+		.catch(function(err) {
+			// If an error occurred, send it to the client
+			res.json(err);
+		});
+});
 
 //Scrapping data from a site
 router.get("/scrape", function(req, res) {
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,40 +5,44 @@ var getComments = function(thisId) {
 	});
 };
 
-// $(document).on("click", "#commentButton", function() {
-// 	var thisId = $(this).attr("data-id");
-// 	// Now make an ajax call for the Article
-// 	getComments(thisId);
-// });
-
 // When you click the save comment button
-$(document).on("click", "#saveComment", function() {
+$(document).on("click", "#submitComment", function(event) {
+	event.preventDefault();
 	// Grab the id associated with the article from the submit button
 	var thisId = $(this).attr("data-id");
+	var commentTitle = $("#comment-title").val();
+	var commentText = $("#comment-text").val();
+
+	if (!commentText && !commentTitle) {
+		alert("Please enter a title and comment!");
+	} else {
+		$.ajax({
+			method: "POST",
+			url: "/articles/" + thisId,
+			data: {
+				// Value taken from title input
+				title: commentTitle,
+				// Value taken from comment textarea
+				text: commentText,
+				article: thisId
+			}
+		})
+			// With that done
+			.then(function(data) {
+				// Log the response
+				// console.log(data);
+				// Empty the comments section
+				$(".modal-title").val("");
+				$("#comment-title").val("");
+				$("#comment-text").val("");
+				// $("#current-title").empty();
+				// $("#current-comment").empty();
+				// $("#current-title").append(data.comment.title);
+				// $("#current-comment").append(data.comment.text);
+				// getComments(thisId);
+				location.reload();
+			});
+	}
 
 	// Run a POST request to change the comment, using what's entered in the inputs
-	$.ajax({
-		method: "POST",
-		url: "/articles/" + thisId,
-		data: {
-			// Value taken from title input
-			title: $("#comment-title").val(),
-			// Value taken from comment textarea
-			text: $("#comment-text").val()
-		}
-	})
-		// With that done
-		.then(function(data) {
-			// Log the response
-			// console.log(data);
-			// Empty the comments section
-			$(".modal-title").val("");
-			$("#comment-title").val("");
-			$("#comment-text").val("");
-			// $("#current-title").empty();
-			// $("#current-comment").empty();
-			// $("#current-title").append(data.comment.title);
-			// $("#current-comment").append(data.comment.text);
-			getComments(thisId);
-		});
 });
